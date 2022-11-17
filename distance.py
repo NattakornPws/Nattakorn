1e7ee00e61482c6b7858cb3a9fd352b9680f60d2
Nattakorn <nattakorn.pws@gmail.com>
2022-11-17 15:09:52 -0500

    third commit

--- distance.py
+++ distance.py
@@ -4,7 +4,7 @@ def distance(pointA, pointB):
     return ((pointA[0] - pointB[0])**2 + (pointA[1] - pointB[1])**2)**0.5
 
 if __name__ == '__main__':
-    pointA = (1,7)
-    pointB = (1,10)
+    pointA = (20,30)
+    pointB = (50,70)
     d = distance(pointA, pointB)
     print(d)
